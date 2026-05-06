98610c2ddce5a60472bbbe1d53f58fa0579af026
Xxcuties <ulyafaroha3@gmail.com>
2026-05-06 12:35:15 +0700

    Update and rename .gitkeep to gfx.js

---
+++ gfx/gfx.js
@@ -0,0 +1,5 @@
+const gfxWorks = [
+  { id: 1, title: "Jingliu", file: "gfx/jingliu.jpg", category: "poster" },
+  { id: 2, title: "Suisei", file: "gfx/suisei.jpg", category: "fanart" },
+  // dst...
+]
