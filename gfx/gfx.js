75362ca7fe264eefc7745d34654ea649154a8da5
Xxcuties <ulyafaroha3@gmail.com>
2026-05-06 19:40:22 +0700

    Update gfx.js

--- gfx/gfx.js
+++ gfx/gfx.js
@@ -1,5 +1,9 @@
 const gfxWorks = [
-  { id: 1, title: "Rio (BLuearchive)", file: "gfx/RIoooo.png", category: "poster" },
+  { id: 1, title: "Rio (Blue Archive)", file: "gfx/RIoooo.png", category: "poster" },
   { id: 2, title: "Bronya", file: "gfx/Bronya mar 2026.png", category: "poster" },
-  // dst...
-]
+  { id: 3, title: "Chisa", file: "gfx/Chisa wuwa.png", category: "fanart" },
+  { id: 4, title: "Firefly", file: "gfx/firefly.png", category: "fanart" },
+  { id: 5, title: "Korwa", file: "gfx/korwa for instagram.png", category: "poster" },
+  { id: 6, title: "Mitsurii", file: "gfx/mitsurii.png", category: "fanart" },
+  { id: 7, title: "Nazi uwu", file: "gfx/nazi uwu.png", category: "fanart" },
+];
