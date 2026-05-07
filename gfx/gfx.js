f74d723e3f423e3b4b943a74eab6560b85358e5b
Xxcuties <ulyafaroha3@gmail.com>
2026-05-07 17:22:40 +0700

    update folder gfx

--- gfx/gfx.js
+++ gfx/gfx.js
@@ -1,9 +1,9 @@
 const gfxWorks = [
-  { id: 1, title: "Rio (Blue Archive)", file: "gfx/RIoooo.png", category: "poster" },
-  { id: 2, title: "Bronya", file: "gfx/Bronya mar 2026.png", category: "poster" },
-  { id: 3, title: "Chisa", file: "gfx/Chisa wuwa.png", category: "poster" },
-  { id: 4, title: "Firefly", file: "gfx/firefly.png", category: "poster" },
-  { id: 5, title: "Korwa", file: "gfx/korwa for instagram.png", category: "poster" },
-  { id: 6, title: "Mitsurii", file: "gfx/mitsurii.png", category: "poster" },
-  { id: 7, title: "Nazi uwu", file: "gfx/nazi uwu.png", category: "poster" },
+  {id: 1, title: "Rio", file: "gfx/foto/RIoooo.png", category: "poster" },
+  { id: 2, title: "Bronya", file: "gfx/foto/Bronya mar 2026.png", category: "poster" },
+  { id: 3, title: "Chisa", file: "gfx/foto/Chisa wuwa.png", category: "poster" },
+  { id: 4, title: "Firefly", file: "gfx/foto/firefly.png", category: "poster" },
+  { id: 5, title: "Korwa", file: "gfx/foto/korwa for instagram.png", category: "poster" },
+  { id: 6, title: "Mitsurii", file: "gfx/foto/mitsurii.png", category: "poster" },
+  { id: 7, title: "Nazi uwu", file: "gfx/foto/nazi uwu.png", category: "poster" },
 ];
