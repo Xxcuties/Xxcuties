21162c02224729f4553ecdd8f951f446bfdaa196
Xxcuties <ulyafaroha3@gmail.com>
2026-05-07 21:19:40 +0700

    Update edu.js

--- edu/edu.js
+++ edu/edu.js
@@ -1,11 +1,29 @@
 const eduWorks = [
-  {
-    id: 1,
-    title: "Latihan Soal SD",
-    desc: "Soal latihan untuk SD — Bahasa, Matematika, IPA",
-    level: "SD",
-    type: "free",
-    filePdf: "edu/pdf/latihan-sd.pdf",
-    fileWord: "edu/word/latihan-sd.docx"
-  },
+    {
+        id: 1,
+        title: "Latihan Soal SD",
+        desc: "test file (jangan download soalnya kosongan!!)",
+        level: "SD",
+        type: "free",
+        filePdf: "edu/pdf/latihan-sd.pdf",
+        fileWord: "edu/word/latihan-sd.docx"
+    },
+    {
+        id: 2,
+        title: "Foods (Super Easy)",
+        desc: "Vocabulary makanan sederhana untuk anak SD",
+        level: "SD",
+        type: "free",
+        filePdf: "edu/pdf/foods (super easy).pdf",
+        fileWord: "edu/word/foods (super easy).docx"
+    },
+    {
+        id: 3,
+        title: "Foods",
+        desc: "Worksheet vocabulary makanan",
+        level: "SD",
+        type: "free",
+        filePdf: "edu/pdf/foods.pdf",
+        fileWord: "edu/word/foods.docx"
+    },
 ];
